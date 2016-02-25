302a4e35803a3ee003b57e1e63f2abfd76184d23
Lakhpat <lakhpat@Lakhpats-iMac.local>
2016-02-25 04:45:04 -0800

    Initial Commit

---
+++ AGoodStart/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  AGoodStart
+//
+//  Created by Lakhpat on 25/02/16.
+//  Copyright (c) 2016 lakhpat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
